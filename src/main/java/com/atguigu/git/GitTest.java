0d8422c824799dc404fda9b2fb0d7206b5a56efb
liuhanxing <2394246254@qq.com>
2022-05-17 23:11:31 +0800

    second

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,9 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git1");
+        System.out.println("hello Git2");
+        System.out.println("hello Git3");
+        System.out.println("hello Git4");
     }
 }
